a0c11c1db795565c0c4fc9af93cbc4f8e0a03c83
Alexander Richey <alexander.richey@gmail.com>
2023-08-06 21:08:01 -0400

    frill -> canny

--- frontend/src/components/GiveFeedbackButton.jsx
+++ frontend/src/components/GiveFeedbackButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "@chakra-ui/react";
 
 export default function GiveFeedback({ children }) {
   return (
-    <Link href="https://linksort.frill.co/b/90p331mj/feature-ideas/idea/new" isExternal>
+    <Link href="https://linksort.canny.io/" isExternal>
       {children}
     </Link>
   );
