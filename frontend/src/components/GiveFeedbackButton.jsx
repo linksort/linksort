e42c81e504633d0769417581d4bc9b8add657e11
Alexander Richey <alexander.richey@gmail.com>
2023-08-07 20:32:53 -0400

    make feedback button point to a more obvious page

--- frontend/src/components/GiveFeedbackButton.jsx
+++ frontend/src/components/GiveFeedbackButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "@chakra-ui/react";
 
 export default function GiveFeedback({ children }) {
   return (
-    <Link href="https://linksort.canny.io/" isExternal>
+    <Link href="https://linksort.canny.io/feedback" isExternal>
       {children}
     </Link>
   );
